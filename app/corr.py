bfddb013282bbb9fe7c89b15e26033429964cee3
panda-with-issues <ryoda94@gmail.com>
2025-09-23 17:42:35 +0200

    migliorato l'html, reso responsive, migliorata la gestione degli errori

--- app/corr.py
+++ app/corr.py
@@ -11,46 +11,34 @@ from app.auth import login_required
 
 bp = Blueprint('corr', __name__, url_prefix='/corr')
 
-@bp.route('/inserisci', methods=('GET', 'POST'))
+@bp.route('/', methods=('GET', 'POST'))
 @login_required
 def inserisci():
-  if (session.get('to_insert')):
-    session.pop('to_insert')
-    
   # Va castato a list perché scalars ritorna un iteratore che consuma i dati quando ci iteri sopra, quindi non posso
   # iterarci due volte senza rifare la query
   mercati = list(db.session.scalars(db.select(Mercati).where(or_(Mercati.is_attuale==True, Mercati.is_evento==True))))
   mercati_nomi = set([ mercato.nome for mercato in mercati ])
   mercati_dict = [ { 'mercato': mercato.nome, 'giorno': mercato.giorno } for mercato in mercati ]
 
+  error = {}
   if request.method == 'POST':
     for key, val in request.form.items():
       session[key] = val
-    
+          
     error, corrispettivo = validate_input(request, mercati_nomi)
 
-    if error is None:
-      cassa_sballata = False
-
-      # alert se cassa > 3
-      if corrispettivo.cassa not in [ f'Cassa {i}' for i in range(1, 4) ]:
-        cassa_sballata = True
-        to_insert = { column.key: getattr(corrispettivo, column.key) for column in corrispettivo.__mapper__.columns }
-        session['to_insert'] = to_insert
-        return render_template('corr/inserisci.html',
-                               confirmation=True, corrispettivo=corrispettivo,
-                               cassa_sballata=cassa_sballata,
-                               mercati=mercati_dict, mercati_nomi=mercati_nomi)
-      
+    if not error:
       try:
         db.session.add(corrispettivo)
         db.session.commit()
 
         user = session['username']
         session.clear()
+
         inserted = { column.key: getattr(corrispettivo, column.key) for column in corrispettivo.__mapper__.columns }
         inserted['data'] = datetime.datetime.strftime(inserted['data'], '%d-%m-%y')
         inserted['ts'] = datetime.datetime.strftime(inserted['ts'], '%d-%m-%y %H:%M')
+
         session['inserted'] = inserted
         session['username'] = user
         return redirect(url_for('corr.success'))
@@ -59,148 +47,207 @@ def inserisci():
         db.session.rollback()
         e = e._message()
         if 'UNIQUE' in e and 'Corrispettivi.data' in e and 'Corrispettivi.mercato' in e:
-          error = f'Esiste già un corrispettivo per il mercato {corrispettivo.mercato} svoltosi il {corrispettivo.data}'
+          error['message'] = f'Esiste già un corrispettivo per il mercato {corrispettivo.mercato} svoltosi il {corrispettivo.data}.'
         else:
-          error = f'Qualcosa è andato storto: {e}'
+          error['message'] = f'Errore di integrità nel database.<br>Dettagli errore: <em>{e}</em>.'
 
       except exc.SQLAlchemyError as e:
         db.session.rollback()
-        error = str(e)
-
-    flash(error)
+        error['message'] = f'Errore nel database.<br>Dettagli errore: <em>{e}</em>.'
  
   return render_template('corr/inserisci.html',
-                         confirmation=False, today=datetime.date.today(),
-                         mercati=mercati_dict, mercati_nomi=mercati_nomi)
+                         today = datetime.date.today(),
+                         mercati = mercati_dict,
+                         mercati_nomi = mercati_nomi,
+                         error = error)
 
 def validate_input(req, mercati):
   corrispettivo = Corrispettivi(ts=datetime.datetime.now(), inserito_da=g.user.username)
+  error = {}
   
   # data
   try:
     data = datetime.datetime.strptime(request.form['data'], '%Y-%m-%d').date()
+    if (data > corrispettivo.ts.date()):
+      error['data'] = 'La data non può essere successiva a oggi.'
   except:
-    return 'La data inserita non è valida', corrispettivo
-  
-  if (data > corrispettivo.ts.date()):
-    return 'La data non può essere successiva a oggi', corrispettivo
+    error['data'] = 'La data inserita non è valida.'
+
   corrispettivo.data = data
 
   # mercati
   mercato = request.form['mercato']
-
   if mercato not in mercati:
-    return 'Mercato non valido', corrispettivo
+    error['mercato'] = 'Mercato non valido.'
+
   corrispettivo.mercato = mercato
 
   # giorno_mercato
   giorno_mercato = request.form['giorno_mercato']
   weekdays = [ 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica' ]
   if giorno_mercato not in weekdays:
-    return 'Giorno della settimana non valido', corrispettivo
+    error['giorno_mercato'] = 'Giorno della settimana non valido.'
+
   corrispettivo.giorno_mercato = giorno_mercato
 
   # controlliamo che la data inserita corrisponda al giorno del mercato inserito
   if (weekdays[corrispettivo.data.weekday()] != corrispettivo.giorno_mercato):
-    return f'Il {datetime.datetime.strftime(corrispettivo.data, "%d-%m-%y")} non era {corrispettivo.giorno_mercato.lower()}', corrispettivo
+    error['data'] = error['data'] if 'data' in error else ''
+    error['giorno_mercato'] = error['giorno_mercato'] if 'giorno_mercato' in error else ''
+    error['message'] = f'Il {datetime.datetime.strftime(corrispettivo.data, "%d-%m-%y")} non era {corrispettivo.giorno_mercato.lower()}.'
   
-  # controlliamo che il mercato sia attuale. Dovrebbe essere già garantito dalla query su `mercati`
+  # controlliamo che il mercato sia attuale. Dovrebbe essere già garantito dalla query su 'mercati'
   mercato = db.session.scalar(db.select(Mercati).where(Mercati.nome == mercato and Mercati.giorno == giorno_mercato))
   assert mercato
   assert bool(mercato.is_attuale) ^ mercato.is_evento
 
   # cassa
   cassa = request.form['cassa']
-  if not re.fullmatch(r"Cassa [1-9]", cassa):
-    return 'La cassa non è valida', corrispettivo
+  if not re.fullmatch(r'Cassa [1-3]', cassa):
+    error['cassa'] = 'La cassa non è valida.'
+
   corrispettivo.cassa = cassa
 
   # reparti
-  reparti = [ reparto for reparto in request.form.keys() if 'reparto' in reparto ]
-  somma = 0
-  for i, rep in enumerate(reparti):
+  reparti = [ k for k in request.form.keys() if 'reparto' in k ]
+  somma_reparti = 0
+  for i, reparto in enumerate(reparti):
     try:
-      reparto = decimal.Decimal(request.form[rep] if request.form[rep] != '' else 0)
+      tot = decimal.Decimal(request.form[reparto] if request.form[reparto] != '' else 0)
+      if tot.as_tuple().exponent < -2:
+        error[f'reparto{i+1}'] = f'Il totale del Reparto {i+1} non può avere più di due cifre decimali.'
+
+      if tot < 0:
+        error[f'reparto{i+1}'] = f'Il totale del Reparto {i+1} non può essere negativo.'
+
+      setattr(corrispettivo, reparto, tot)
+   
+      somma_reparti += tot
+
     except:
-      return f'Valore invalido per il reparto {i+1}', corrispettivo
-    
-    if reparto.as_tuple().exponent < -2:
-     return f'Reparto {i+1} ha più di due cifre decimali', corrispettivo
-    
-    if reparto < 0:
-      return f'Reparto {i+1} non può essere negativo', corrispettivo
-    
-    setattr(corrispettivo, rep, reparto)
-    
-    somma += reparto
+      error[f'reparto{i+1}'] = f'Valore invalido per il totale del Reparto {i+1}.'
+
+  # quantità
+  quantitas = [ k for k in request.form.keys() if 'quantita' in k ]
+  somma_quantita = 0
+  for i, quantita in enumerate(quantitas):
+    try:
+      tot = int(request.form[quantita]) if request.form[quantita] != '' else 0
+
+      if tot < 0:
+        error[f'quantita{i+1}'] = f'La quantità del Reparto {i+1} non può essere negativa.'
+
+      setattr(corrispettivo, quantita, tot)
+   
+      somma_quantita += tot
+
+    except:
+      error[f'quantita{i+1}'] = f'Valore invalido per la quantità del Reparto {i+1}.'
   
-  # totale
+  # totale reparti
   try:
-    totale = decimal.Decimal(request.form['totale'])
+    rep_totale = decimal.Decimal(request.form['totale'])
+    if rep_totale.as_tuple().exponent < -2:
+      error['totale'] = 'Il totale non può avere più di due cifre decimali.'
+    
+    if rep_totale < 0:
+      error['totale'] = 'Il totale non può essere negativo.'
+    
+    if somma_reparti != rep_totale:
+      error['reparto1'] = error['reparto1'] if 'reparto1' in error else ''
+      error['reparto2'] = error['reparto2'] if 'reparto2' in error else ''
+      error['reparto3'] = error['reparto3'] if 'reparto3' in error else ''
+      error['reparto4'] = error['reparto4'] if 'reparto4' in error else ''
+      error['reparto5'] = error['reparto5'] if 'reparto5' in error else ''
+      error['totale'] = 'La somma dei reparti non coincide col totale.'
+  
+  except Exception as e:
+    error['totale'] = 'Valore invalido per il totale.'
+
+  # n. pezzi
+  try:
+    q_totale = int(request.form['quantita_totale'])
+    
+    if q_totale < 0:
+      error['q_totale'] = 'Il n. pezzi non può essere negativo.'
+    
+    if somma_quantita != q_totale:
+      error['quantita1'] = error['quantita1'] if 'quantita1' in error else ''
+      error['quantita2'] = error['quantita2'] if 'quantita2' in error else ''
+      error['quantita3'] = error['quantita3'] if 'quantita3' in error else ''
+      error['quantita4'] = error['quantita4'] if 'quantita4' in error else ''
+      error['quantita5'] = error['quantita5'] if 'quantita5' in error else ''
+      error['quantita_totale'] = 'La somma delle quantita dei reparti non coincide col n. pezzi.'
+  
   except:
-    return 'Valore invalido per il totale', corrispettivo
-  
-  if totale.as_tuple().exponent < -2:
-    return f'Totale ha più di due cifre decimali', corrispettivo
-  
-  if totale < 0:
-    return f'Totale non può essere negativo', corrispettivo
-  
-  if somma != totale:
-    return 'La somma dei reparti non coincide col totale', corrispettivo
+    error['quantita_totale'] = 'Valore invalido per il totale.'
 
   # resta da controllare se esiste già uno scontrino con i cinque reparti uguali e dare un warning
   # perché potrebbe trattarsi di un duplicato
 
-  return None, corrispettivo
+  return error, corrispettivo
 
-@bp.route('inserisci/success')
+@bp.route('/success')
 @login_required
 def success():
   inserted = session.pop('inserted')
   return render_template('corr/success.html', inserted=inserted)
 
-@bp.route('inserisci/insert')
-@login_required
-def insert():
-  c = session.pop('to_insert')
-  corrispettivo = Corrispettivi(
-    data = datetime.datetime.strptime(c['data'], '%a, %d %b %Y %H:%M:%S %Z').date(),
-    mercato = c['mercato'],
-    ts = c['ts'],
-    inserito_da = c['inserito_da'],
-    giorno_mercato=c['giorno_mercato'],
-    cassa = c['cassa'],
-    reparto1 = c['reparto1'],
-    reparto2 = c['reparto2'],
-    reparto3 = c['reparto3'],
-    reparto4 = c['reparto4'],
-    reparto5 = c['reparto5']
-  )
+# if (session.get('to_insert')):
+#    session.pop('to_insert')
+# cassa_sballata = False
 
-  try:
-    db.session.add(corrispettivo)
-    db.session.commit()
+# # alert se cassa > 3
+# if corrispettivo.cassa not in [ f'Cassa {i}' for i in range(1, 4) ]:
+#   cassa_sballata = True
+#   to_insert = { column.key: getattr(corrispettivo, column.key) for column in corrispettivo.__mapper__.columns }
+#   session['to_insert'] = to_insert
+#   return render_template('corr/inserisci.html',
+#                          confirmation=True, corrispettivo=corrispettivo,
+#                          cassa_sballata=cassa_sballata,
+#                          mercati=mercati_dict, mercati_nomi=mercati_nomi)
 
-    user = session['username']
-    session.clear()
-    inserted = { column.key: getattr(corrispettivo, column.key) for column in corrispettivo.__mapper__.columns }
-    inserted['data'] = datetime.datetime.strftime(inserted['data'], '%d-%m-%y')
-    inserted['ts'] = datetime.datetime.strftime(inserted['ts'], '%d-%m-%y %H:%M')
-    session['inserted'] = inserted
-    session['username'] = user
-    return redirect(url_for('corr.success'))
+# @bp.route('/insert')
+# @login_required
+# def insert():
+#   c = session.pop('to_insert')
+#   corrispettivo = Corrispettivi(
+#     data = datetime.datetime.strptime(c['data'], '%a, %d %b %Y %H:%M:%S %Z').date(),
+#     mercato = c['mercato'],
+#     ts = c['ts'],
+#     inserito_da = c['inserito_da'],
+#     giorno_mercato=c['giorno_mercato'],
+#     cassa = c['cassa'],
+#     reparto1 = c['reparto1'],
+#     reparto2 = c['reparto2'],
+#     reparto3 = c['reparto3'],
+#     reparto4 = c['reparto4'],
+#     reparto5 = c['reparto5']
+#   )
+
+#   try:
+#     db.session.add(corrispettivo)
+#     db.session.commit()
+
+#     user = session['username']
+#     session.clear()
+#     inserted = { column.key: getattr(corrispettivo, column.key) for column in corrispettivo.__mapper__.columns }
+#     inserted['data'] = datetime.datetime.strftime(inserted['data'], '%d-%m-%y')
+#     inserted['ts'] = datetime.datetime.strftime(inserted['ts'], '%d-%m-%y %H:%M')
+#     session['inserted'] = inserted
+#     session['username'] = user
+#     return redirect(url_for('corr.success'))
   
-  except exc.IntegrityError as e:
-    db.session.rollback()
-    e = e._message()
-    if 'UNIQUE' in e and 'Corrispettivi.data' in e and 'Corrispettivi.mercato' in e:
-      flash(f'Esiste già un corrispettivo per il mercato {corrispettivo.mercato} svoltosi il {corrispettivo.data}')
-    else:
-      flash(f'Qualcosa è andato storto: {e}')
-    return redirect(url_for('corr.inserisci'))
+#   except exc.IntegrityError as e:
+#     db.session.rollback()
+#     e = e._message()
+#     if 'UNIQUE' in e and 'Corrispettivi.data' in e and 'Corrispettivi.mercato' in e:
+#       flash(f'Esiste già un corrispettivo per il mercato {corrispettivo.mercato} svoltosi il {corrispettivo.data}')
+#     else:
+#       flash(f'Qualcosa è andato storto: {e}')
+#     return redirect(url_for('corr.inserisci'))
 
-  except exc.SQLAlchemyError as e:
-    db.session.rollback()
-    flash(str(e))
-    return redirect(url_for('corr.inserisci'))
+#   except exc.SQLAlchemyError as e:
+#     db.session.rollback()
+#     flash(str(e))
+#     return redirect(url_for('corr.inserisci'))
---
+++ app/static/script/corr.js
@@ -0,0 +1,86 @@
+'use strict'
+
+const mercatoInput = document.getElementById('mercato')
+const giornoInput = document.getElementById('giorno_mercato')
+const errDialog = document.getElementById('error-dialog')
+
+const inputs = document.querySelectorAll('input, select')
+
+// const confirmationArticle = document.querySelector('.confirmation')
+const defaultChildren = [ ...giornoInput.children ]
+defaultChildren.forEach(opt => {
+  opt.selected = false
+})
+
+const errors = Object.keys(serverError)
+if (errors.length) {
+  errDialog.showModal()
+  inputs.forEach(el => {
+    if (errors.includes(el.name)) {
+      el.classList.add('input-error')
+    }
+  })
+}
+
+inputs.forEach(el => {
+  el.addEventListener('change', e => {
+    if (el.classList.contains('input-error')) {
+      el.classList.remove('input-error')
+    }
+  })
+})
+
+mercatoInput.addEventListener('change', filter_giorni)
+mercatoInput.addEventListener('change', removeBlank, {once: true})
+
+if (session?.mercato) {
+  mercatoInput.dispatchEvent(new Event('change'))
+  mercatoInput.firstChild.selected = false
+  for (const opt of giornoInput.children) {
+    if (opt.value === session.giorno_mercato) {
+      opt.selected = true
+    }
+  }
+}
+
+function filter_giorni (e) {
+  giornoInput.replaceChildren(...defaultChildren)
+
+  const mercato = e.target.value
+
+  const giorni = [ 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica' ]
+  const idxs = []
+  mercati.forEach(m => {
+    if (m.mercato === mercato) {
+      idxs.push(giorni.indexOf(m.giorno))
+    }
+  })
+  idxs.sort()
+
+  const newChildren = []
+  idxs.forEach(idx => {
+    newChildren.push(defaultChildren[idx])
+  })
+  giornoInput.replaceChildren(...newChildren)
+  giornoInput.firstChild.selected = true
+}
+
+function removeBlank (e) {
+  [ ...e.target.children ].forEach(child => {
+    if (child.value === '') {
+      e.target.removeChild(child)
+    }
+  })
+}
+
+// if (confirmationArticle) {
+//   setTimeout(() => {
+//     confirmationArticle.classList.add('confirmation-active')
+//     const descendants = confirmationArticle.querySelectorAll('*')
+//     for (const el of descendants) {
+//       if (el.classList.contains('hidden')) {
+//         el.classList.add('visible')
+//       }
+//     }
+//   }, 100)
+// }
